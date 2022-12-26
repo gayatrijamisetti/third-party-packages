63e3bdeea8102808c9d3e3b0e19ac8bf7245f4f2
gayatrijamisetti <gayatrijamisetti@gmail.comgit>
2022-12-26 09:34:43 +0000

    Video-player-controls

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -1,14 +1,33 @@
+import {Component} from 'react'
 import ReactPlayer from 'react-player'
 
 import './index.css'
 
-const VideoPlayer = () => (
-  <div className="video-container">
-    <h1 className="heading">Video Player</h1>
-    <div className="responsive-container">
-      <ReactPlayer url="https://youtu.be/YE7VzlLtp-4" />
-    </div>
-  </div>
-)
+const videoUrl = 'https://youtu.be/YE7VzlLtp-4'
+class VideoPlayer extends Component {
+  state = {
+    isPlaying: false,
+  }
+
+  onClickPlay = () => {
+    this.setState(prevState => ({isPlaying: !prevState.isPlaying}))
+  }
+
+  render() {
+    const {isPlaying} = this.state
+    const btnText = isPlaying ? 'pause' : 'play'
+    return (
+      <div className="video-container">
+        <h1 className="heading">Video Player</h1>
+        <div className="responsive-container">
+          <ReactPlayer url={videoUrl} playing={isPlaying} controls />
+        </div>
+        <button className="button" type="button" onClick={this.onClickPlay}>
+          {btnText}
+        </button>
+      </div>
+    )
+  }
+}
 
 export default VideoPlayer
